fd3378afa6958ebfeebc991f8887ab3bb3cb20c4
Mohit Bharat Vishwakarma <vishwakarmaaakash386@gmail.com>
2024-12-16 18:00:21 +0530

    Few components added
    cart, footer &  success

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Nav, Navbar, Col, Row } from "react-bootstrap";
+
+import "./footer.css";
+
+const Footer = () => {
+
+  return (
+    <>
+      <Col className="footer-container">
+        
+        <p className="footerText">Made By</p>
+        <Navbar className="justify-content-center footerText">
+          <Nav>
+            <Nav.Item>
+              <Nav.Link target="_blank" href="https://github.com/Mohit-205/">
+                Mohit
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link target="_blank" href="">
+                
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link target="_blank" href="">
+                
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link target="_blank" href="">
+                
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </Col>
+    </>
+  );
+};
+
+export default Footer;
