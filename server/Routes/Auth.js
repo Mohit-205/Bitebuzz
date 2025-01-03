b6cfae07f9a2490de0446e69c3774f5551ae5f98
Mohit Bharat Vishwakarma <vishwakarmaaakash386@gmail.com>
2025-01-03 18:14:19 +0530

    update

--- server/Routes/Auth.js
+++ server/Routes/Auth.js
@@ -61,23 +61,5 @@ router.get("/favorites", async (req, res) => {
   }
 });
 
-// POST /api/auth/add-favorite
-router.post('/api/auth/add-favorite', authenticateToken, async (req, res) => {
-  const { recipe } = req.body; // Recipe object to add
-  const userId = req.user.id; // Extract user ID from token
-  
-  try {
-    const user = await User.findById(userId);
-    if (!user) return res.status(404).send("User not found");
-
-    user.favorites.push(recipe); // Add recipe to favorites
-    await user.save();
-
-    res.status(200).json({ message: 'Recipe added to favorites' });
-  } catch (error) {
-    res.status(500).json({ message: 'Error adding recipe to favorites', error });
-  }
-});
-
 
 module.exports = router;
