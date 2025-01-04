311c36cd8b4cac6af610b441b355f52d83012778
Mohit Bharat Vishwakarma <vishwakarmaaakash386@gmail.com>
2025-01-04 19:17:02 +0530

    update

--- server/Models/User.js
+++ server/Models/User.js
@@ -17,7 +17,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
   favoriteRecipes: {
-    type: [String], // Array of recipe IDs
+    type: [Object], // Array of recipe IDs
     default: [],
   },
 });
