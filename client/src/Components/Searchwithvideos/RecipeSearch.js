edc01e1920cf543c3306cc3bb1eafee61b5b3dd5
Mohit Bharat Vishwakarma <108185967+Mohit-205@users.noreply.github.com>
2025-06-27 21:14:58 +0530

    Update RecipeSearch.js

--- client/src/Components/Searchwithvideos/RecipeSearch.js
+++ client/src/Components/Searchwithvideos/RecipeSearch.js
@@ -13,8 +13,8 @@ const RecipeSearch = () => {
   const [favorites, setFavorites] = useState([]);
 
   // API credentials for Edamam
-  const appId = 'REMOVED';
-  const appKey = 'REMOVED';
+  const appId = 'appId';
+  const appKey = 'appKey';
 
   // Function to fetch recipes based on search term or random recipes
   const fetchRecipes = async (query) => {
@@ -35,7 +35,7 @@ const RecipeSearch = () => {
 
       // If there is a search term, fetch related YouTube videos
       if (query) {
-        const youtubeApiUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query} recipe&key=REMOVED&maxResults=20`;
+        const youtubeApiUrl = `youtubeapiurl`;
         const youtubeResponse = await fetch(youtubeApiUrl);
         const youtubeData = await youtubeResponse.json();
         setVideoLinks(youtubeData.items);
